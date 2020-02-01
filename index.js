2f32e5f58ac0e7ad14c708072494e1c31fbb3f2e
Carl Becker <klequis@gmail.com>
2020-02-01 08:06:49 -0800

    -wip

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { DATA_COLLECTION_NAME } from 'db/constants'
 import { mergeAll } from 'ramda'
 import loadData from './load-data'
 // eslint-disable-next-line
-import { green, greenf, redf } from 'logger'
+import { green, greenf, redf, yellow } from 'logger'
 
 const readRules = async () => {
   const rules = await fs.promises.readFile('rules.json')
@@ -12,31 +12,85 @@ const readRules = async () => {
   return json
 }
 
-// rename action-value
-const renameAction = async r => {
-  // const f = await find(DATA_COLLECTION_NAME, { description: { $in: [/^C/] } })
-  // const f = await find(DATA_COLLECTION_NAME, { description: { $regex: `^${r.criteria[0].value}` } })
-  // green('f', f)
-  // green('r.actionValue', r.actionValue)
-  const from = r.criteria[0].value
-  const to = r.actionValue
-  const um = await updateMany(
+const wrappedFind = async filter => {
+  green('filter', filter)
+  return find(
     DATA_COLLECTION_NAME,
-    { description: { $regex: `^${from}` } },
-    { description: to }
+    filter,
+    {},
+    // { caseLevel: true, locale: 'en_US' }
+    { locale: 'en', strength: 2 }
   )
-
-  greenf(`Renamed ${um.modifiedCount} documents`)
-  greenf('    from: ', from)
-  greenf('    to: ', to)
 }
 
 const operationBeginsWith = (field, value) => {
   return { [field]: { $regex: `^${value}`, $options: 'im' } }
 }
 
+const operationContains = (field, value) => {
+  return { [field]: { $regex: `${value}`, $options: 'im' } }
+}
+
 const operationEquals = (field, value) => {
   return { [field]: { $eq: value } }
+}
+
+const conditionBuilder = criteria => {
+  // takes a single criteria object
+  const { field, operation, value } = criteria
+  switch (operation) {
+    case 'beginsWith':
+      return operationBeginsWith(field, value)
+    case 'equals':
+      return operationEquals(field, value)
+    case 'contains':
+      return operationContains(field, value)
+    default:
+      redf(
+        'deleteAction ERROR: ',
+        `operation ${operation} not covered in switch`
+      )
+      throw new Error('conditionBuilder ERROR: unknown operation')
+  }
+}
+
+const filterBuilder = criteria => {
+  if (criteria.length === 1) {
+    return conditionBuilder(criteria[0])
+  } else {
+    const x = mergeAll(
+      criteria.map(c => {
+        // const { field, operation, value } = c
+        const ret = conditionBuilder(c)
+        green('ret', ret)
+        return ret
+      })
+    )
+    return { $and: [x] }
+  }
+}
+
+
+const renameAction = async r => {
+  const { criteria, actionValue } = r
+  const from = criteria[0].value
+  const to = actionValue
+
+  const filter = filterBuilder(criteria)
+  // tmp code
+  const f = await wrappedFind(filter)
+  green('rename - documents', f.length)
+  // tmp code
+
+  // const um = await updateMany(
+  //   DATA_COLLECTION_NAME,
+  //   filterBuilder(criteria),
+  //   { description: to }
+  // )
+
+  // greenf(`Renamed ${um.modifiedCount} documents`)
+  // greenf('    from: ', from)
+  // greenf('    to: ', to)
 }
 
 const a = {
@@ -58,97 +112,45 @@ const b = {
   ]
 }
 
-const conditionBuilder = criteria => {
-  // takes a single criteria object
-  const { field, operation, value } = criteria
-  switch (operation) {
-    case 'beginsWith':
-      return operationBeginsWith(field, value)
-    case 'equals':
-      return operationEquals(field, value)
-    default:
-      redf(
-        'deleteAction ERROR: ',
-        `operation ${operation} not covered in switch`
-      )
-      throw new Error('conditionBuilder ERROR: unknown operation')
-  }
+const c = {
+  $and: [
+    { description: { $regex: 'Bel Air II', $options: 'im' } },
+    { typeOrig: 'BILLPAY' }
+  ]
 }
 
-const filterBuilder = criteria => {
-  if (criteria.length === 1) {
-    return conditionBuilder(criteria[0])
-  } else {
-    return { $and: [] }
-  }
-}
 
 // delete [none]
 const deleteAction = async r => {
   const { criteria } = r
 
-  let filter
-  if (criteria.length > 1) {
-    green('criteria', criteria)
-    // const works = {
-    //   $and: [
-    //     { description: { $regex: '^NY STATE' } },
-    //     { description: { $regex: 'NYSTTAXRFD' } }
-    //   ]
-    // }
+  const filter = filterBuilder(criteria)
+  
+  const f = await wrappedFind(filter)
+  green('delete - documents returned', f.length)
 
-    const x = mergeAll(
-      criteria.map(c => {
-        // const { field, operation, value } = c
-        const ret = conditionBuilder(c)
-        green('ret', ret)
-        return ret
-      })
-    )
-    filter = { $and: [x] }
-    // green('works', works)
-    green('filter', filter)
-    // green('y', y)
-  } else {
-
-    // return
-    // const criteria = r.criteria[0]
-    // const { field, operation, value } = criteria
-    // switch (operation) {
-    //   case 'beginsWith':
-    //     filter = operationBeginsWith(field, value)
-    //     break
-    //   case 'equals':
-    //     filter = operationEquals(field, value)
-    //     break
-    //   default:
-    //     redf(
-    //       'deleteAction ERROR: ',
-    //       `operation ${operation} not covered in switch`
-    //     )
-    // }
-  }
-  // green('filter', filter)
-  green('filter', filter)
-  const f = await find(
-    DATA_COLLECTION_NAME,
-    filter,
-    {},
-    // { caseLevel: true, locale: 'en_US' }
-    { locale: 'en', strength: 2 }
-  )
-  // green('f', f)
-  green('documents returned', f.length)
-
-  const dm = await deleteMany(DATA_COLLECTION_NAME, filter, {}, { locale: 'en', strength: 2 })
-  greenf(`Deleted ${dm.deletedCount} documents`)
-  greenf(`    filter: ${filter}`)
+  // const dm = await deleteMany(DATA_COLLECTION_NAME, filter, {}, { locale: 'en', strength: 2 })
+  // greenf(`Deleted ${dm.deletedCount} documents`)
+  // greenf(`    filter: ${filter}`)
 }
 
 // strip  [none]
-const stripAction = async r => {}
+const stripAction = async r => {
+  
+  const { criteria, expectRows } = r
+  const f = await wrappedFind(filterBuilder(criteria))
+  // green('f', f)
+  expectRows === f.length
+    ? greenf(`OK: expected: ${expectRows}, actual: ${f.length}`)
+    : redf(`ERROR: expected: ${expectRows}, actual: ${f.length}`)
+  green('f.length', f.length)
+
+}
+
 // categorize category1, [category2]
-const categorizeAction = async r => {}
+const categorizeAction = async r => {
+  
+}
 
 const main = async () => {
   await loadData()
@@ -171,10 +173,10 @@ const main = async () => {
         // renameAction(r)
         break
       case 'delete':
-        deleteAction(r)
+        // deleteAction(r)
         break
       case 'strip':
-        // stripAction(r)
+        stripAction(r)
         break
       case 'categorize':
         // categorizeAction(r)
